561d5b46b2dfd9029db59dd6991e03d577af805c
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:29:24 +0100

    Add project files.

---
+++ OpenLab 02.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OpenLab_02._4_vlastne
+{
+    class Program
+    {
+        public static int legsCount(int chickens, int cows, int pigs)
+        {
+            int count = ((chickens * 2) + (cows * 4) + (pigs * 4));
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("počet chickens :");
+            var input = Console.ReadLine();
+            Console.WriteLine("počet cows :");
+            var input2 = Console.ReadLine();
+            Console.WriteLine("počet pigs :");
+            var input3 = Console.ReadLine();
+            int chickens  = Int32.Parse(input);
+            int cows = Int32.Parse(input2);
+            int pigs = Int32.Parse(input3);
+            int count = legsCount(chickens, cows, pigs);
+            Console.WriteLine($"Súčet je {count}");
+        }
+    }
+}
